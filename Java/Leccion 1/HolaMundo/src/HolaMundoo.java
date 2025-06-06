ce2b3e64bd829a403ae47d65358a09a4a698b33d
Valentin Castillo <colocross97@gmail.com>
2025-06-06 00:24:02 -0300

    Borre el directorio practica y comence el trabajo en class git y cargando comandos

--- Java/Leccion 1/HolaMundo/src/HolaMundoo.java
+++ Java/Leccion 1/HolaMundo/src/HolaMundoo.java
@@ -1,7 +1,8 @@
 
+import java.util.Scanner;
 public class HolaMundoo {   
     public static void main(String[] args) {
-        System.out.println("Hola Mundo desde Java");
+        /*System.out.println("Hola Mundo desde Java");
         
         int miVariable = 10;
         System.out.println(miVariable);
@@ -12,5 +13,46 @@ public class HolaMundoo {
         System.out.println(miVariableCadena);
         miVariableCadena = "Sigamos creciendo en programacion";
         System.out.println(miVariableCadena);
+        */
+        
+        //Var -inferencia de tipos de Java
+        /*var miVariableEntera2 = 10;
+        var miVariableCadena2 = "Seguimos estudiando";
+        System.out.println("miVariableEntera2 = " + miVariableEntera2);
+        System.out.println("miVariableCadena2 = " + miVariableCadena2);
+        //soutv + tab
+        //Para ejecutar = shift + f6
+        
+        //Reglas para definir una variable en Java
+        
+        var usuario = "Diego";
+        var titulo = "Ingeniero";
+        var union = titulo + " " + usuario;
+        System.out.println("union = " + union);
+        
+        
+        var a = 8;
+        var b = 4;
+        System.out.println(usuario + (a + b));
+        
+        //Ejercicio: Carecteres Especiales con Java
+        var nombre = "Nazareno";
+        System.out.println("`\nNueva linea: \n"+nombre); //Diagonal inversa y letra n
+        System.out.println("Tabulador: \t"+nombre);//Tabulador un espacio para centrar
+        System.out.println("\t\t.:MENU:.");
+        System.out.println("Retroseso: \b\b"+nombre);//Caracter de Retroceso
+        System.out.println("Comillas simples: \'"+nombre+"\'");
+        System.out.println("Comillas dobles: \""+nombre+"\"");
+        */
+        
+        //Clase Scanner
+        Scanner entrada = new Scanner(System.in); 
+        System.out.println("Digite su nombre: ");
+        var usuario2 = entrada.nextLine();
+        System.out.println("usuario2 = " + usuario2);
+        System.out.println("Escriba el titulo: ");
+        var titulo2 = entrada.nextLine();
+        System.out.println("Resultado: "+titulo2+" "+usuario2);
+        
     }        
 }
