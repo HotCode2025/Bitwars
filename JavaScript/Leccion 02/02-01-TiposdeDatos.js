2db2237a7ae9e0a9d36b6a12c1ed4c8b29457172
Valentin Castillo <colocross97@gmail.com>
2025-06-06 00:26:42 -0300

    trabajo en class git y cargando comandos

---
+++ JavaScript/Leccion 02/02-01-TiposdeDatos.js
@@ -0,0 +1,19 @@
+//Tipos de Datos en JavaScript
+/*
+La sintaxis en lo que es comentarios 
+es muy similar a la de Java
+realmente diriamos que es identica
+*/
+var nombre = 'Valentin'; // Tipo Str
+console.log(nombre);
+
+var numero = 3000; // Tipo numerico
+console.log(numero);
+
+var objeto = {
+    nombre : "Valentin",
+    apellido : "Castillo",
+    telefono : "2604038884"
+}
+
+console.log(objeto);
