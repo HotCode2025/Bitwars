e2d57a94e1870c9f6e0493dfa8e7db20c227f16a
Valentin Castillo <colocross97@gmail.com>
2025-06-07 22:20:43 -0300

    Ingreso algunos archivos de Java de la Leccion 1

--- Java/Leccion 1/HolaMundo/src/HolaMundoo.java
+++ Java/Leccion 1/HolaMundo/src/HolaMundoo.java
@@ -46,13 +46,47 @@ public class HolaMundoo {
         */
         
         //Clase Scanner
-        Scanner entrada = new Scanner(System.in); 
+        /*Scanner entrada = new Scanner(System.in); 
         System.out.println("Digite su nombre: ");
         var usuario2 = entrada.nextLine();
         System.out.println("usuario2 = " + usuario2);
         System.out.println("Escriba el titulo: ");
         var titulo2 = entrada.nextLine();
         System.out.println("Resultado: "+titulo2+" "+usuario2);
+        */
+        /*
+        byte numEnteroByte = 127;
+        System.out.println("numEnteroByte = " + numEnteroByte);
+        System.out.println("Valor minimo del Byte:"+ Byte.MIN_VALUE);
+        System.out.println("Valor maximo del Byte:"+ Byte.MAX_VALUE);
         
-    }        
+        
+        short numEnteroShort = 32767;
+        System.out.println("numEnteroShort = " + numEnteroShort);
+        System.out.println("Valor minimo del Short:"+ Short.MIN_VALUE);
+        System.out.println("Valor maximo del Short:"+ Short.MAX_VALUE);
+        
+        int numEnteroInt = 2147483647;
+        System.out.println("numEnteroInt = " + numEnteroInt);
+        System.out.println("Valor minimo del int:"+ Integer.MIN_VALUE);
+        System.out.println("Valor maximo del int:"+ Integer.MAX_VALUE);
+        
+        long numEnteroLong = 9223372036854775807L;
+        System.out.println("numEnteroLong = " + numEnteroLong);
+        System.out.println("Valor minimo del int:"+ Long.MIN_VALUE);
+        System.out.println("Valor maximo del int:"+ Long.MAX_VALUE);
+        */
+        
+        float numFloat = 3.4028235E38F;
+        System.out.println("numFloat = " + numFloat);
+        System.out.println("El valor minimo de float:"+ Float.MIN_VALUE);
+        System.out.println("El valor maximo de float:"+ Float.MAX_VALUE);
+        
+        double numDouble = 1.7976931348623157E308D;
+        System.out.println("numDouble = " + numDouble);
+        System.out.println("El valor minimo de double:"+ Double.MIN_VALUE);
+        System.out.println("El valor maximo de double:"+ Double.MAX_VALUE);
+        
+        
+    }       
 }
---
+++ Java/Leccion 1/HolaMundo/src/holamundo/HolaaMundo.java
@@ -0,0 +1,10 @@
+
+package holaamundo;
+
+
+public class HolaaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola mundo desde Java");  
+    }
+    
+}
