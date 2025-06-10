4dd0afb4ac1469d76002e15bfcdee6530ea43056
colo-castillo <colocross97@gmail.com>
2025-06-10 00:41:08 -0300

    creando el header del portafolio web

--- JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
+++ JavaScript/Leccion03/03-01-OperadoresJavaScript-UJS.js
@@ -23,3 +23,43 @@ console.log(nombre2);
 const apellido2 = "Lepes";
 //apellido2 = "Lepes"; una constante no puede ser modificada
 console.log(apellido2)
+
+//Ejercicio: Si el padre puede asistir al juego de su hijo
+let vacaciones = true, diaDescanso = false;
+if(vacaciones || diaDescanso){
+    console.log("El padre puede asistir el juego de su hijo")
+}
+else{
+    console.log("El padre No puede asistir al juego de su hijo")
+}
+
+//Operador ternario
+let resultado2 = 3 > 2 ? "Verdadero" : "Falso";
+console.log(resultado2)
+let numero = 12; 
+resultado2 = numero % 2 == 0 ? "Es un numero PAR" : "Es un numero IMPAR";
+console.log(resultado2)
+
+
+
+//Convertir String a Numbrer
+let miNumero = "21"; //Es una cadena
+console.log(typeof miNumero);
+let edad2 = Number(miNumero); //Esta es una funcion
+console.log(typeof edad2);
+//Funcion isNaN
+if(isNaN(edad2)){ //No es un numero = is Not a Number(devuelve un resultado booleano)
+    console.log("Esta variable no contiene solo numeros");
+}
+else{
+    if (edad2 >= 18){
+    console.log("Puede votar");
+    }
+    else{
+        console.log("Muy joven para votar")
+    }
+}
+
+//Operador ternario
+let resultado3 = edad2 >= 18 ? "Puede votar" : "Muy joven para votar";
+console.log(resultado3);
