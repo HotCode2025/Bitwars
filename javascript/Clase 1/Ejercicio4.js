3c7eeb25f47150750c5d6e4b6a918a4b04bdeb64
Francesco Cornachione <cornachofrance@gmail.com>
2025-08-20 00:44:31 -0300

    Subir Ejercicio 4 Javascript y borrar carpeta innecesaria

---
+++ javascript/Clase 1/Ejercicio4.js
@@ -0,0 +1,4 @@
+// Como preguntar si es un Array o Arreglo
+console.log(Array.isArray(autos)); //Devuelve un booleano
+
+console.log(autos instanceof Array); //Preguntamos si la variable es una instancia de la calse Array
