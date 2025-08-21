424dd7e61ee25730e799becf677b1b9813833ba7
Francesco Cornachione <cornachofrance@gmail.com>
2025-08-21 11:35:18 -0300

    Subir clase 2 JavaScript

---
+++ javascript/Clase 2/3. Modificar los elementos de un arreglo.js
@@ -0,0 +1,14 @@
+const autos = ['Ferrari', 'Renault', 'BMW'];
+
+// Modificar elementos de un arreglo
+autos[1] = 'Volvo';
+
+// Agregar elementos al arreglo
+autos.push('Audi');
+
+// Otras formas de agregar elementos al arreglo
+autos[autos.length]  = 'Porsche';
+
+// Otra forma de agregar elementos (CUIDADO)
+
+autos[6] = 'Lamborghini'; // (Pueden quedar espacios en blanco)
