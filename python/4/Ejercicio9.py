c0e7d342e16c0b5336a666cb566684d60330b2aa
Tomas Vilche <agustinvilchebaron@gmail.com>
2025-09-01 22:19:55 -0300

    Ejercicios 8 y 9

---
+++ python/4/Ejercicio9.py
@@ -0,0 +1,7 @@
+# ejercicio 9 frase sin espacios y contar
+
+frase = input("mete una frase: ")
+
+sin_espacios = frase.replace(" ", "")
+print("frase final:", sin_espacios)
+print("caracteres:", len(sin_espacios))
