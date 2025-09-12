202c87039b5654c0e895dcfc17f4da43df8a0a17
Maria Jose Sela <mselaalias@gmail.com>
2025-09-11 22:23:52 -0300

    Subir el ejercicio 5.1 de Java

---
+++ java/Clase 5/5.1/Ejercicio51.java
@@ -0,0 +1,16 @@
+
+package ejercicio5.pkg1;
+
+public class Ejercicio51 {
+//atributos de la clase
+    int a;
+    int b;
+    
+    //método
+    public void sumarNumeros() {
+        int resultado = a + b;
+        System.out.println("resultado= "+resultado);
+    
+    }
+    
+}
