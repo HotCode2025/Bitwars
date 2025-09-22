1664979688f8eaa515581b5032932dbdaf27a9f2
Francesco Cornachione <cornachofrance@gmail.com>
2025-09-22 19:00:44 -0300

    Clase 3 - Javascript

---
+++ javascript/Clase 3 - Funciones/1.js
@@ -0,0 +1,8 @@
+// Definir la función
+function miFuncion(a, b){
+  const resultado = a + b
+  console.log("Resultado: " + resultado);
+}
+
+// Llamar a la función
+miFuncion(2, 5)
---
+++ javascript/Clase 3 - Funciones/3.js
@@ -0,0 +1,5 @@
+// Función tipo expresión o anónima
+let x = function(a, b){return a + b}; // Necesita cierre con punto y coma
+resultado = x(2, 4); // Se llama con la variable (x) y los paréntesis
+
+console.log(resultado);
